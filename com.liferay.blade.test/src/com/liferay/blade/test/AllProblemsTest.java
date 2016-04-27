398a85e591a21f80d8b7b657bea9ae744d677c62
Lovett Li <lovett.li@liferay.com>
2016-04-27 09:33:12 +0800

    [migrator] update problem number

--- com.liferay.blade.test/src/com/liferay/blade/test/AllProblemsTest.java
+++ com.liferay.blade.test/src/com/liferay/blade/test/AllProblemsTest.java
@@ -43,7 +43,7 @@ public class AllProblemsTest {
 				.findProblems(new File(
 						"../com.liferay.blade.upgrade.liferay70/projects/"), new NullProgressMonitor());
 
-		final int expectedSize = 293;
+		final int expectedSize = 1234;
 		final int size = problems.size();
 
 		if (size != expectedSize) {
