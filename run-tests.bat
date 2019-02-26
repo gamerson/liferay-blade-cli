1bf3c83d9380ffd50603ba7d9053b88bcfdb3c92
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2019-02-26 10:32:42 -0600

    modify run-tests.bat

--- run-tests.bat
+++ run-tests.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 CALL rmdir /s/q "%userprofile%\.m2\repository\com\liferay\blade\com.liferay.blade.extensions.maven.profile"
-CALL gradlew.bat clean
-CALL gradlew.bat :extensions:maven-profile:publishToMavenLocal
-CALL gradlew.bat -PmavenLocal -continue check smokeTests --scan
+CALL gradlew.bat --stop
+CALL gradlew.bat --no-daemon clean
+CALL gradlew.bat --no-daemon :extensions:maven-profile:publishToMavenLocal
+CALL gradlew.bat --no-daemon -PmavenLocal -continue check smokeTests --scan
+CALL gradlew.bat --stop
