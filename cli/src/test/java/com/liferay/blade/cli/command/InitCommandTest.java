166482973d4615eb799d2b94f79fd3198873403c
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2019-05-13 18:25:11 -0500

    Upgrade to com.liferay.project.templates 4.2.15

--- cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
+++ cli/src/test/java/com/liferay/blade/cli/command/InitCommandTest.java
@@ -460,7 +460,21 @@ public class InitCommandTest {
 					newproject, "settings.gradle"
 				).toPath()));
 
-		Assert.assertTrue(contents, contents.contains("1.10"));
+		boolean contentContainsVersion = contents.contains(_GRADLE_PLUGINS_WORKSPACE_VERSION);
+
+		if (!contentContainsVersion) {
+			StringBuilder sb = new StringBuilder("Error checking com.liferay.gradle.plugins.workspace version.");
+
+			sb.append(System.lineSeparator());
+
+			sb.append("Expected " + _GRADLE_PLUGINS_WORKSPACE_VERSION);
+
+			sb.append(System.lineSeparator());
+
+			sb.append(contents);
+
+			Assert.fail(String.valueOf(sb));
+		}
 	}
 
 	@Test
@@ -577,6 +591,8 @@ public class InitCommandTest {
 
 		GradleRunnerUtil.verifyBuildOutput(projectPath + "/foo", "foo-1.0.0.jar");
 	}
+
+	private static final String _GRADLE_PLUGINS_WORKSPACE_VERSION = "2.0.2";
 
 	private File _extensionsDir = null;
 	private File _workspaceDir = null;
