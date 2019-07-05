9928a2afdd6a8b6c76ad2ec3088f9e5423522637
Terry Jia <terry.jia@liferay.com>
2019-07-05 17:53:02 +0800

    BLADE-458 add LSP support and add one simple completion for portal.properties file

---
+++ cli/src/main/java/com/liferay/blade/cli/languageserver/LiferayTextDocumentService.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.blade.cli.languageserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.CompletionParams;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.TextDocumentService;
+
+/**
+ * @author Terry Jia
+ */
+public class LiferayTextDocumentService implements TextDocumentService {
+
+	public LiferayTextDocumentService(LiferayLanguageServer liferayLanguageServer) {
+	}
+
+	@Override
+	public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(
+		CompletionParams completionParams) {
+
+		TextDocumentIdentifier textDocument = completionParams.getTextDocument();
+
+		List<CompletionItem> completionItems = new ArrayList<>();
+
+		try {
+			URI uri = new URI(textDocument.getUri());
+
+			File file = new File(uri);
+
+			String fileName = file.getName();
+
+			if (fileName.startsWith("portal") && fileName.endsWith("properties")) {
+				Class<?> clazz = getClass();
+
+				InputStream in = null;
+
+				try {
+					in = clazz.getResourceAsStream("/portal.properties");
+
+					Properties properties = new Properties();
+
+					properties.load(in);
+
+					Enumeration keys = properties.keys();
+
+					while (keys.hasMoreElements()) {
+						Object key = keys.nextElement();
+
+						completionItems.add(new CompletionItem(String.valueOf(key)));
+					}
+				}
+				catch (IOException ioe) {
+				}
+				finally {
+					if (in != null) {
+						try {
+							in.close();
+						}
+						catch (IOException ioe) {
+						}
+					}
+				}
+			}
+		}
+		catch (URISyntaxException urise) {
+		}
+
+		return CompletableFuture.supplyAsync(() -> Either.forLeft(completionItems));
+	}
+
+	@Override
+	public void didChange(DidChangeTextDocumentParams didChangeTextDocumentParams) {
+	}
+
+	@Override
+	public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) {
+	}
+
+	@Override
+	public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) {
+	}
+
+	@Override
+	public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) {
+	}
+
+}
