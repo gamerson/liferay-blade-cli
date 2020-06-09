ae0de004216021cf63ea8dddf384601d0072ac80
Simon.Jiang <simon.jiang@liferay.com>
2020-06-09 23:09:36 +0800

    BLADE-553 get liferay version from workspace product info

--- cli/src/main/java/com/liferay/blade/cli/gradle/GradleWorkspaceProvider.java
+++ cli/src/main/java/com/liferay/blade/cli/gradle/GradleWorkspaceProvider.java
@@ -27,7 +27,6 @@ import com.liferay.blade.cli.util.ProductInfo;
 
 import java.io.File;
 import java.io.FilenameFilter;
-
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
